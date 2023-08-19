2a6be92d710343e9bc03d8fad6a054a691440879
Felix <felixjgca@gmail.com>
2023-08-19 02:00:18 -0300

    sad

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,7 +3,6 @@ import { Routes as ReactDomRoutes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import PageNotFound from "../pages/PageNotFound/PageNotFound";
 import SobreNosotros from "../pages/SobreNosotros/SobreNosotros";
-import ProductosRoutes from "../pages/ProductosRoutes/ProductosRoutes";
 import ContactoRoutes from "../pages/ContactoRoutes/ContactoRoutes";
 import React from "react";
 
@@ -12,7 +11,6 @@ const Routes = () => {
     <ReactDomRoutes>
       <Route path="/" element={<Home />} />
       <Route path="/sobrenosotros" element={<SobreNosotros />} />
-      <Route path="/productosroutes" element={<ProductosRoutes />} />
       <Route path="/contactoroutes" element={<ContactoRoutes />} />
       <Route path="*" element={<PageNotFound />} />
     </ReactDomRoutes>
